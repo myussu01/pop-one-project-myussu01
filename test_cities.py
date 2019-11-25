0a4205bab09c406c41d292cdd863129b49f9af51
Mohamed Yussuf <myussu01@mail.bbk.ac.uk>
2019-11-25 21:12:46 +0000

    Final testing

--- test_cities.py
+++ test_cities.py
@@ -21,7 +21,7 @@ def test_swap_cities():
                  ("Minnesota", "Saint Paul", 44.95, -93.094)]
 
 
-    assert swap_cities(road_map1, 11.223, 44.67) == (0,0,0)
+    assert swap_cities(road_map1, 11.223, 44.67) == (road_map1,0,0)
 
 def test_shift_cities():
     road_map1 = [("Kentucky", "Frankfort", 38.197274, -84.86311), \
