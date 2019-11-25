47f4b3a5b09e86c269cf7e3961160a9879a4df69
Mohamed Yussuf <myussu01@mail.bbk.ac.uk>
2019-11-25 15:33:11 +0000

    hchanged test file first commit first commit

--- test_cities.py
+++ test_cities.py
@@ -9,13 +9,19 @@ def test_compute_total_distance():
     
     assert compute_total_distance(road_map1)==\
            pytest.approx(9.386+18.496+10.646, 0.01)
+    '''add your tests'''
 
-    '''add your further tests'''
 
 def test_swap_cities():
     '''add your tests'''
 
 def test_shift_cities():
+    road_map1 = [("Kentucky", "Frankfort", 38.197274, -84.86311), \
+                 ("Delaware", "Dover", 39.161921, -75.526755), \
+                 ("Minnesota", "Saint Paul", 44.95, -93.094)]
+    road_map2 = []
+
+    assert shift_cities(road_map1) == road_map2
     '''add your tests'''
 
 
